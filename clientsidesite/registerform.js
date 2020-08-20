70f8ed0626d1455a641d785149bf66a97380e6ec
sandy13c <adilfanta@gmail.com>
2020-08-20 16:59:29 -0400

    Update registerform.js

--- clientsidesite/registerform.js
+++ clientsidesite/registerform.js
@@ -1,34 +1,35 @@
-
 window.onload = function() {
  var registeration = new Vue({
     el:'#registeration',
     data: {
       errors:[],
-      first_name:'',
-      last_name:'',
-      user_name:'',
-      password:'',
-      confirmpassword:''
+      first_name:null,
+      last_name:null,
+      user_name:null,
+      password:null,
+      confirmpassword:null
     },
     methods: {
       Register(a) {
            this.errors = [];
-           if (this.first_name === '') {
+           if (!this.first_name) {
              this.errors.push("Please enter firstname");
           }
-           if (this.last_name === '') {
+           if (!this.last_name) {
              this.errors.push("Please enter lastname");
            }
-          if (this.user_name === '') {
+          if (!this.user_name) {
             this.errors.push("Please enter username");
           }
-          if(this.password === '') {
+          if(!this.password) {
             this.errors.push("Please enter password");
           }
-          if (this.confirmpassword === '') {
+          if (!this.confirmpassword) {
             this.errors.push("Please re-enter password");
           }
-
+          if(this.firstname && this.lastname &&
+          this.user_name && this.password && this.confirmpassword) {
+               confirm("Thank you for registering");
                axios.post('http://localhost:3100/register/addUser',
               {
                      first_name: this.first_name,
@@ -39,15 +40,12 @@ window.onload = function() {
             }).then((response) =>{
                this.first_name='';
                this.last_name='';
-               this.user_name:'';
-               this.password:'';
-               this.confirmpassword:'';
-               confirm("Thank you for registering");
-            }).catch( error=> {
-              console.log('error: ' + error);
-
+               this.user_name='';
+               this.password='';
+               this.confirmpassword='';
+            }).catch(error=> {
             });
-
+          }
             a.preventDefault();
       }
 
@@ -56,3 +54,4 @@ window.onload = function() {
 
 
 }
+
