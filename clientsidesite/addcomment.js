1d942b56742ced4f84ccfd182f6f0d442d716c52
sandy13c <adilfanta@gmail.com>
2020-08-20 17:05:32 -0400

    Update addcomment.js

--- clientsidesite/addcomment.js
+++ clientsidesite/addcomment.js
@@ -2,9 +2,11 @@ window.onload = () => {
  const section = new Vue({
    el:"#section",
    data: {
+    errors:[],
     comments:[],
-    newName:'',
-    newComment:'',
+    newComnum:null,
+    newName:null,
+    newComment:null,
     //loading: false
     //submitting:false
 
@@ -21,23 +23,37 @@ window.onload = () => {
       });
     },
      addComment() {
+       this.errors=[];
+       if(!this.newComnum) {
+         this.errors.push("Enter comment number");
+       }
+       if(!this.newName) {
+         this.errors.push("Enter your name");
+       }
+       if(!this.newComment) {
+         this.errors.push("Enter your comments please");
+       }
+       if(this.newComnum && this.newName && this.newComment) {
        axios.post('http://localhost:3100/comments/add',{
+          com_id:this.newComnum,
           name:this.newName,
           details:this.newComment
        }).then((response) => {
             const data = response.data;
             this.comments.push(data);
+            this.newComnum='';
             this.newName='';
             this.newComment='';
-            //this.submitting=true;
-
        });
+     }
      },
-      deleteComment(com_id) {
+
+     deleteComment(com_id) {
          axios.delete('http://localhost:3100/comments/'+ com_id).then(() => {
             this.showComments();
          });
      }
+
   }
  });
 }
